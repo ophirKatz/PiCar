fa5583e58b3cbf584173e4b8d19d3d503a0d9bc3
unknown <ophirk8396@gmail.com>
2021-11-03 10:05:07 +0200

    added aviv alg

---
+++ aviv.py
@@ -0,0 +1,46 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from __future__ import print_function
+import cv2 as cv
+import argparse
+
+import numpy as np
+
+from Line import Line
+import lane_detection
+
+max_lowThreshold = 100
+window_name = 'Edge Map'
+title_trackbar = 'Min Threshold:'
+ratio = 3
+kernel_size = 3
+
+
+parser = argparse.ArgumentParser(description='Code for Canny Edge Detector tutorial.')
+parser.add_argument('--input', help='Path to input image.', default='lubin34.jpg')
+args = parser.parse_args()
+src = cv.imread(cv.samples.findFile(args.input))
+src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+img_blur = cv.blur(src_gray, (3,3))
+
+low_threshold = 100
+detected_edges = cv.Canny(img_blur, low_threshold, low_threshold*ratio, kernel_size)
+
+detected_edges = detected_edges[240:480, 0:640]
+
+lines = cv.HoughLinesP(detected_edges, 2, np.pi / 180, 1, 15, 5)
+
+for line in lines:
+    x1, y1, x2, y2 = line[0]
+    cv.line(detected_edges, (x1,y1), (x2,y2), (255,255,255), 3)
+
+cv.imshow(window_name, detected_edges)
+cv.namedWindow(window_name)
+cv.waitKey()
+
+
+
+
